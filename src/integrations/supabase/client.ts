4fbdf2f18054842cf5dcffe86b30e0ef259c4ef8
bbbhuvanbab-tech <bb.bhuvanbab@gmail.com>
2026-06-01 15:33:47 +0530

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -5,7 +5,7 @@ export const isSupabaseConfigured = true;
 
 export const supabase = createClient<Database>(
   'https://mwklngfmvalxwjdomtxa.supabase.co',
-  'YOUR_ACTUAL_ANON_KEY_HERE',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13a2xuZ2ZtdmFseHdqZG9tdHhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyNjA0MzIsImV4cCI6MjA5NDgzNjQzMn0.fbMoc3KGcDrbQ5hvH5EwwVv0VZR3foduVhy7I-IPxgQ',
   {
     auth: {
       storage: localStorage,
